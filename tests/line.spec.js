d3edacc693a4db9429bbd2b7e5557e2d6bcc1f0b
jobikajo-bit <jobikajo@gmail.com>
2026-01-21 15:21:07 +0530

    add changes

---
+++ tests/line.spec.js
@@ -0,0 +1,73 @@
+import { test, expect } from '@playwright/test';
+import { allure } from 'allure-playwright';
+const { AllureUtils } = require('../utils/allureUtils');
+
+test('Snaptrude', async ({ page }) => {
+ /* await page.addInitScript(() => {
+    "prevent-gpu-access-modal":'true'
+  });*/
+  await page.addInitScript(() => {
+  localStorage.setItem('prevent-gpu-access-modal', 'true');
+});
+
+    test.setTimeout(90000);
+
+   await page.goto('https://staging.snaptrude.xyz/login',{waitUntil:'domcontentloaded'});
+    
+   expect(page.locator('img[alt="Snaptrude"]'));
+   
+   await page.fill('//input[@id="identifier-field"]', 'automation1@snaptrude.com');
+   
+   await page.locator('//span[text()="Continue"]').click();
+await page.fill('//input[@id="password-field"]', 'Automation1@123');
+    
+await page.locator('//span[text()="Continue"]').click();
+await page.waitForLoadState('domcontentloaded');
+//await page.click('.checkbox.grid.place-items-center.bg-fullWhite.h-\\[12px\\]');
+//await page.click('div[class="flex h-5 w-5 cursor-pointer items-center justify-center p-2 active:scale-95"]');
+
+await page.locator('//button[@id="ai-workflow-button"]').click();
+await page.fill('//input[@name="projectName"]', 'Test Project');
+await page.press('//input[@name="projectName"]', 'Enter');
+//await page.click('//div[normalize-space()="cm"]//div[normalize-space()="m"]');
+//await page.click('//span[text()="Next"]');
+await page.locator('//div[text()="Start on blank canvas"]').click();
+await page.waitForLoadState('domcontentloaded');
+AllureUtils.takeScreenshot(page, 'Snaptrude Blank Canvas Loaded');
+
+await page.locator('//img[@alt="draw"]').click();
+await page.locator('//canvas[@id="canvas"]').click({
+
+    position: {
+      x: 415,
+      y: 247
+    }
+  });
+await page.locator('#canvas').click({
+    position: {
+      x: 529,
+      y: 243
+    }
+  });
+await page.locator('#canvas').click({
+    position: {
+      x: 529,
+      y: 349
+    }
+  });
+await page.locator('#canvas').click({
+    position: {
+      x: 411,
+      y: 346
+    }
+  });
+await page.locator('#canvas').click({
+    position: {
+      x: 417,
+      y: 250
+    }
+  });
+
+AllureUtils.takeScreenshot(page, 'Snaptrude Line Drawn');
+
+});
